568c3415312756d0409df1c6ae9915cc07370552
epiphannie <amheal01@gmail.com>
2019-04-20 15:04:49 -0400

    Practice Video, Conditionals and Loops

---
+++ CsharpCL/ConditionalLoopPractice.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace Treehouse.PracticeSession
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // TODO Declare a boolean variable named "keepGoing"
+            // and assign it a value of "true". Use this variable to 
+            // know when to exit the while loop.
+            var keepGoing = true;
+
+            // TODO Define a while loop.
+            // Keep looping as long as the variable "keepGoing" has a value of "true".
+            while (keepGoing)
+            {
+                Console.Write("Enter a number: ");
+                var entry = Console.ReadLine();
+
+                if (entry == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    var number = Double.Parse(entry);
+                    var square = number * number;
+                    Console.WriteLine(number + " multiplied by itself equals " + square + ".");
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+                // TODO Prompt the user with the text "Enter a number:" 
+                // and assign their value to an "entry" string variable.
+                
+                // TODO If the user entered the text "quit"...
+
+                    // TODO Exit the program.
+
+                // TODO Else the user didn't enter the text "quit"...
+
+                    // TODO Parse the user's entry to an integer
+                    
+                    // TODO Square the user's provided number 
+                    // (i.e. multiply the number by itself).
+                    
+                    // TODO Output the result.
+                    // Example: "The square of 2 is 4." or 
+                    // "2 multiplied by itself is equal to 4."
+            
+            // TODO Output the text "Goodbye!" after exiting the loop.
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
