1b65b840beb20365d68641eab2a28e95df72e878
epiphannie <amheal01@gmail.com>
2019-04-22 21:06:30 -0400

    Object Oriented, classes & methods

---
+++ ObjectOriented/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    public class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+
+            Point point = new Point(4, 2);
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+
+            Console.WriteLine(point.DistanceTo(5, 5));
+
+        }
+    }
+}
